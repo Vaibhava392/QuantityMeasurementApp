ae10f8f98a11e5a72b15c6dd7557da5e4c8cb804
vaibhava392 <vaibhavamukund@gmail.com>
2026-04-28 09:00:37 +0530

    UC3: Generic Quantity Class for DRY Principle

--- App/src/QuantityMeasurementApp.java
+++ App/src/QuantityMeasurementApp.java
@@ -1,70 +1,65 @@
 public class QuantityMeasurementApp {
 
-    public static class Feet {
-        private final double value;
+    public enum LengthUnit {
+        FEET(12.0),
+        INCH(1.0);
 
-        public Feet(double value) {
+        private final double conversionFactor;
+
+        LengthUnit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
+        }
+
+        public double convertToBase(double value) {
+            return value * this.conversionFactor;
+        }
+    }
+
+    public static class Quantity {
+        private final double value;
+        private final LengthUnit unit;
+
+        public Quantity(double value, LengthUnit unit) {
+            if (unit == null) {
+                throw new IllegalArgumentException("Unit cannot be null");
+            }
             this.value = value;
+            this.unit = unit;
         }
 
         @Override
         public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Feet feet = (Feet) obj;
-            return Double.compare(feet.value, value) == 0;
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Quantity that = (Quantity) obj;
+            double value1 = this.unit.convertToBase(this.value);
+            double value2 = that.unit.convertToBase(that.value);
+
+            return Double.compare(value1, value2) == 0;
         }
 
         @Override
         public int hashCode() {
-            return Double.hashCode(value);
+            return Double.hashCode(unit.convertToBase(value));
         }
-    }
-
-    public static class Inches {
-        private final double value;
-
-        public Inches(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Inches inches = (Inches) obj;
-            return Double.compare(inches.value, value) == 0;
-        }
-
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
-        }
-    }
-
-    public static boolean compareFeet(double val1, double val2) {
-        Feet f1 = new Feet(val1);
-        Feet f2 = new Feet(val2);
-        return f1.equals(f2);
-    }
-
-    public static boolean compareInches(double val1, double val2) {
-        Inches i1 = new Inches(val1);
-        Inches i2 = new Inches(val2);
-        return i1.equals(i2);
     }
 
     public static void main(String[] args) {
-        double f1 = 1.0, f2 = 1.0;
-        double i1 = 1.0, i2 = 1.0;
+        Quantity oneFoot = new Quantity(1.0, LengthUnit.FEET);
+        Quantity twelveInches = new Quantity(12.0, LengthUnit.INCH);
+        Quantity oneInch = new Quantity(1.0, LengthUnit.INCH);
+        Quantity anotherOneInch = new Quantity(1.0, LengthUnit.INCH);
 
-        boolean feetResult = compareFeet(f1, f2);
-        boolean inchResult = compareInches(i1, i2);
+        System.out.println("Input: Quantity(1.0, \"feet\") and Quantity(12.0, \"inches\")");
+        System.out.println("Output: Equal (" + oneFoot.equals(twelveInches) + ")");
 
-        System.out.println("Input: " + i1 + " inch and " + i2 + " inch");
-        System.out.println("Output: Equal (" + inchResult + ")");
-
-        System.out.println("Input: " + f1 + " ft and " + f2 + " ft");
-        System.out.println("Output: Equal (" + feetResult + ")");
+        System.out.println("Input: Quantity(1.0, \"inch\") and Quantity(1.0, \"inch\")");
+        System.out.println("Output: Equal (" + oneInch.equals(anotherOneInch) + ")");
     }
 }
